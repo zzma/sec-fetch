2f7ed0e553c5cfe63d1c29b455db5b367795064d
zanema <amenaz1@gmail.com>
2018-12-14 11:03:52 -0600

    oakland stuff

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"net/url"
 	"os"
 	"path"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -49,8 +50,8 @@ func (e FetchError) Error() string {
 }
 
 var (
-	MissingDownloadLinkErr = FetchError{Msg:"no pdf download links found on page"}
-	TooManyDownloadLinksErr = FetchError{Msg:"too many pdf download links found on page"}
+	MissingDownloadLinkErr  = FetchError{Msg: "no pdf download links found on page"}
+	TooManyDownloadLinksErr = FetchError{Msg: "too many pdf download links found on page"}
 )
 
 func createConfDirectory(outputDirectory string, conf Conference) (string, error) {
@@ -63,7 +64,6 @@ func createConfDirectory(outputDirectory string, conf Conference) (string, error
 	}
 	return confDirectory, nil
 }
-
 
 func getFullUrl(baseUrl, linkUrl string) (string, error) {
 	var fullUrl string
@@ -144,6 +144,35 @@ func getDownloadUrl(pageUrl string, matcher scrape.Matcher) (string, error) {
 	if len(pageNodes) > 1 {
 		return fileUrl, TooManyDownloadLinksErr
 	}
+
+	if strings.Contains(fileUrl, "www.ieee-security.org") {
+		allVersionsRegex := regexp.MustCompile(`^All [\d]+ versions$`)
+		allVersionsMatcher := func(n *html.Node) bool {
+			if n.DataAtom == atom.A {
+				return allVersionsRegex.MatchString(scrape.Text(n))
+			}
+			return false
+		}
+
+		versionLink, ok := scrape.Find(root, allVersionsMatcher)
+		if !ok { log.Fatalf("no version link found for: %s", fileUrl)}
+		versionUrl, err := getFullUrl(pageUrl, scrape.Attr(versionLink, "href"))
+		if err != nil {
+			return "", err
+		}
+
+		urlMatcher := func(n *html.Node) bool {
+			// must check for nil values
+			if n.DataAtom == atom.A && n.Parent != nil {
+				href := scrape.Attr(n, "href")
+				return strings.HasSuffix(href, ".pdf")  && scrape.Attr(n.Parent, "class") == "gs_or_ggsm" && !strings.Contains(href, "www.ieee-security.org")
+			}
+			return false
+		}
+
+		return getDownloadUrl(versionUrl, urlMatcher)
+	}
+
 	return fileUrl, nil
 }
 
@@ -172,9 +201,33 @@ func getLinks(pageUrl string, matcher scrape.Matcher) ([]string, error) {
 	return pages, nil
 }
 
+
+func getPaperTitles(pageUrl string, matcher scrape.Matcher) ([]string, error) {
+	response, err := http.Get(pageUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	root, err := html.Parse(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// grab all paper titles
+	titleNodes := scrape.FindAll(root, matcher)
+	titles := make([]string, 0)
+	for _, title := range titleNodes {
+		title := scrape.Text(title)
+
+		titles = append(titles, title)
+	}
+
+	return titles, nil
+}
+
 // Pre-main bind flags to variables
 func init() {
-	flag.DurationVar(&config.fetchTimeout, "timeout", 500*time.Millisecond, "timeout between downloading papers")
+	flag.DurationVar(&config.fetchTimeout, "timeout", 2*time.Second, "timeout between downloading papers")
 	flag.StringVar(&config.conferencesFile, "config", "conferences.json", "JSON file listing conferences")
 	flag.StringVar(&config.outputDirectory, "output-dir", "papers", "output directory for storing papers")
 	flag.Parse()
@@ -335,6 +388,116 @@ func main() {
 			default:
 				log.Printf("no parser found for %s", conf.String())
 			}
+		case "Oakland":
+			confDirectory, err := createConfDirectory(config.outputDirectory, conf)
+			if err != nil {
+				log.Fatal(err)
+			}
+			switch {
+			case conf.Year <= 2018 && conf.Year >= 2015:
+				matcher := func(n *html.Node) bool {
+					if n.DataAtom == atom.B && n.Parent != nil {
+						return scrape.Attr(n.Parent, "class") == "list-group-item"
+					}
+					return false
+				}
+
+				titles, err := getPaperTitles(conf.URL, matcher)
+				if err != nil {
+					log.Fatal(err)
+				}
+				for _, title := range titles {
+					// Generate google scholar search URL
+					gScholarSearchTemplate := "https://scholar.google.com/scholar?q="
+					queryString := strings.Replace(title, " ", "+", -1)
+					gScholarUrl, err := url.Parse(gScholarSearchTemplate + queryString)
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					urlMatcher := func(n *html.Node) bool {
+						// must check for nil values
+						if n.DataAtom == atom.A && n.Parent != nil {
+							href := scrape.Attr(n, "href")
+							return strings.HasSuffix(href, ".pdf")  && scrape.Attr(n.Parent, "class") == "gs_or_ggsm"
+						}
+						return false
+					}
+
+					downloadUrl, err := getDownloadUrl(gScholarUrl.String(), urlMatcher)
+					if err != nil {
+						if err == MissingDownloadLinkErr {
+							log.Printf("missing download link for: %s\n", gScholarUrl.String())
+							continue
+						} else if err == TooManyDownloadLinksErr {
+							log.Println(err)
+						} else {
+							log.Fatal(err)
+						}
+					}
+					log.Printf("%s: %s", title, downloadUrl)
+					splitUrl := strings.Split(downloadUrl, "/")
+					filepath := path.Join(confDirectory, splitUrl[len(splitUrl)-1])
+					if strings.Contains(downloadUrl, "www.ieee-security.org") {
+						log.Println("skipping download, since www.ieee-security.org checks JS for download...annoying")
+					} else {
+						downloadFile(downloadUrl, filepath)
+					}
+					time.Sleep(config.fetchTimeout)
+				}
+			case conf.Year <= 2014:
+				matcher := func(n *html.Node) bool {
+					if n.DataAtom == atom.A && n.Parent != nil && n.Parent.Parent != nil {
+						return scrape.Attr(n.Parent.Parent, "class") == "list-group-item"
+					}
+					return false
+				}
+
+				titles, err := getPaperTitles(conf.URL, matcher)
+				if err != nil {
+					log.Fatal(err)
+				}
+				for _, title := range titles {
+					// Generate google scholar search URL
+					gScholarSearchTemplate := "https://scholar.google.com/scholar?q="
+					queryString := strings.Replace(title, " ", "+", -1)
+					gScholarUrl, err := url.Parse(gScholarSearchTemplate + queryString)
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					urlMatcher := func(n *html.Node) bool {
+						// must check for nil values
+						if n.DataAtom == atom.A && n.Parent != nil {
+							return strings.HasSuffix(scrape.Attr(n, "href"), ".pdf") && scrape.Attr(n.Parent, "class") == "gs_or_ggsm"
+						}
+						return false
+					}
+
+					downloadUrl, err := getDownloadUrl(gScholarUrl.String(), urlMatcher)
+					if err != nil {
+						if err == MissingDownloadLinkErr {
+							continue
+						} else if err == TooManyDownloadLinksErr {
+							log.Println(err)
+						} else {
+							log.Fatal(err)
+						}
+					}
+
+					log.Printf("%s: %s", title, downloadUrl)
+					splitUrl := strings.Split(downloadUrl, "/")
+					filepath := path.Join(confDirectory, splitUrl[len(splitUrl)-1])
+					if strings.Contains(downloadUrl, "www.ieee-security.org") {
+						log.Println("skipping download, since www.ieee-security.org checks JS for download...annoying")
+					} else {
+						downloadFile(downloadUrl, filepath)
+					}
+					time.Sleep(config.fetchTimeout)
+				}
+			}
+
+
 		default:
 			log.Printf("no parser found for %s", conf.String())
 		}
